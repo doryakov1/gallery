f7efef0720cdf3b282993b05e788a5a373dcb33f
dor <betshean@walla.com>
2019-05-30 20:39:56 +0300

    v2

--- js/service.js
+++ js/service.js
@@ -2,9 +2,9 @@ var gProjects;
 
 function createProjects() {
     gProjects = [createProject(1, 'Game', 'minesSweeper', 'the old nice game', 'img/mineSweeper.png', '16/5/19', ['Matrix2D', 'Game'], 'https://doryakov1.github.io/gallery/projets/minesweeper/'),
-        createProject(2, 'App', 'Todo', 'Add, Update and Delete YourStuff', 'img/todo2.png', '20/5/19', ['Flex-Table', 'App']),
-        createProject(3, 'Game', 'Guess Who?', 'Guess The Charcater', 'img/guess2.png', '22/5/19', ['Flex-Table-Machine', 'Game']),
-        createProject(4, 'App', 'Book Shoop', 'Add, Update and Delete YourStuff', 'img/bookShop.png', '25/5/19', ['Flex-Table', 'App'])
+        createProject(2, 'App', 'Todo', 'Add, Update and Delete YourStuff', 'img/todo2.png', '20/5/19', ['Flex-Table', 'App'], 'https://doryakov1.github.io/gallery/projets/dor-todo-mvc/'),
+        createProject(3, 'Game', 'Guess Who?', 'Guess The Charcater', 'img/guess2.png', '22/5/19', ['Flex-Table-Machine', 'Game'], 'https://doryakov1.github.io/gallery/projets/GuessMe-Dor/'),
+        createProject(4, 'App', 'Book Shoop', 'Add, Update and Delete YourStuff', 'img/bookShop.png', '25/5/19', ['Flex-Table', 'App'], 'https://doryakov1.github.io/gallery/projets/Books Shop/')
     ]
 }
 
